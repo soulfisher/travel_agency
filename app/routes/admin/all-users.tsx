8feb3e3e28b3c001951028893179dba884b3cb71
Cassandra Cook <cassandracook@gmail.com>
2025-06-20 17:28:45 -0700

    Create dashboard

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,6 +1,15 @@
+import {Header} from "../../../components";
+
 const AllUsers = () => {
     return (
-        <div>Users Table</div>
+        <main className="dashboard wrapper">
+            <Header
+                title="Users Page"
+                description="Check out our current users in real time"
+            />
+
+            All Users Page Contents
+        </main>
     )
 }
 export default AllUsers
